0e7dd3249e603062b0d94bc957c142c093ac59f3
RODRIGO BREZOLIN BUQUERA <81428197+Rodrigo-Brezolin-Buquera@users.noreply.github.com>
2023-03-29 15:54:06 -0300

    color pallete

---
+++ src/constants/colors.tsx
@@ -0,0 +1,12 @@
+type pallete = {
+    [key:string]: string | number
+}
+
+export const colors: pallete = {
+  brown:  "#44281d" ,
+  orange:  "#e4a788",	
+  yellow:  "#f0e14a",	
+  green:  "#97ce4c",	
+  pink:  "#e89ac7",
+  black: "#000000"
+}
